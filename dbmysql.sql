12849ad5dbfdd5bf112a500b6c6d385f43d2f84d
Ricardo Avelino <rick58@ymail.com>
2022-09-04 21:33:32 -0300

    adiciona sql para db mysql

---
+++ dbmysql.sql
@@ -0,0 +1,63 @@
+
+CREATE DATABASE gamagram; 
+
+USE GAMAGRAM;
+
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE usuarios (
+    id int not null auto_increment primary key,
+    nome text not null,
+    imagem text,
+    username varchar(30) not null,
+    email varchar(50) not null,
+    password text not null,
+    site text,
+    bio text,
+    telefone text,
+    genero text
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id int not null auto_increment primary key,
+    usuario_id int not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
+    legenda text not null,
+    foreign key (usuario_id) references usuarios (id)
+);
+
+DROP TABLE IF EXISTS comentarios;
+
+CREATE TABLE comentarios (
+    id int not null auto_increment primary key,
+    post_id int not null,
+    usuario_id int not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
+    conteudo text not null,
+    foreign key (post_id) references posts (id),
+    foreign key (usuario_id) references usuarios (id)
+);
+
+DROP TABLE IF EXISTS fotos;
+
+CREATE TABLE fotos (
+    id int not null auto_increment primary key,
+    post_id int not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
+    imagem text not null,
+    foreign key (post_id) references posts (id)
+);
+
+DROP TABLE IF EXISTS likes;
+
+CREATE TABLE likes (
+    post_id int not null,
+    usuario_id int not null,
+    foreign key (post_id) references posts (id),
+    foreign key (usuario_id) references usuarios (id)
+);
