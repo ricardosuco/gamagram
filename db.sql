fdfbe5aede81f6ec5f1626b422e80a24257b588f
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-09-08 22:26:11 -0300

    Arquivo SQL com os campos atualizados em inglês
    Foi feito atraves da função forward engineering do mysql workbench

---
+++ db.sql
@@ -0,0 +1,118 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+
+
+-- -----------------------------------------------------
+-- Schema gamagram
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `gamagram` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `gamagram` ;
+
+-- -----------------------------------------------------
+-- Table `gamagram`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gamagram`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `image` TEXT NULL DEFAULT NULL,
+  `username` TEXT NOT NULL,
+  `email` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  `site` TEXT NULL DEFAULT NULL,
+  `bio` TEXT NULL DEFAULT NULL,
+  `phone` TEXT NULL DEFAULT NULL,
+  `gender` CHAR(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gamagram`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gamagram`.`posts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `caption` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `usuario_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `posts_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `gamagram`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gamagram`.`comments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gamagram`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `post_id` (`post_id` ASC) VISIBLE,
+  INDEX `usuario_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `comentarios_ibfk_1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `gamagram`.`posts` (`id`),
+  CONSTRAINT `comentarios_ibfk_2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `gamagram`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gamagram`.`photos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gamagram`.`photos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `post_id` INT NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `image` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `post_id` (`post_id` ASC) VISIBLE,
+  CONSTRAINT `fotos_ibfk_1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `gamagram`.`posts` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gamagram`.`likes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gamagram`.`likes` (
+  `post_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  INDEX `post_id` (`post_id` ASC) VISIBLE,
+  INDEX `usuario_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `likes_ibfk_1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `gamagram`.`posts` (`id`),
+  CONSTRAINT `likes_ibfk_2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `gamagram`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
