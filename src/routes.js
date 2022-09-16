7db86395c859b8dc76dae774ac602b2e25b7640a
Ricardo Avelino <rick58@ymail.com>
2022-09-15 23:04:37 -0300

    fix: atualiza rotas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,36 @@
 const express = require('express')
-// const teste = require('./controllers/UserController')
-const { list, show, create, teste, update, deleteUser, login } = require('./controllers/UserController')
+const userController = require('./controllers/userController')
+const commentController = require('./controllers/CommentController')
+const postController = require('./controllers/postController')
+const auth = require('./middleware/auth')
 
 const routes = express();
 
-// User
-routes.get('/', teste)
-// routes.get('/users', UserController.list)
-// routes.get('/username', UserController.show)
-// routes.put('/user', UserController.update)
-routes.post('/register', create)
-routes.post('/login', login)
+routes.post('/register', userController.create)
+routes.post('/login', userController.login)
+
+routes.use(auth)
+
+// Rotas autenticadas
+ 
+//User
+routes.get('/users', userController.list)
+routes.get('/:username', userController.show)
+routes.put('/user', userController.update)
+routes.delete('/user', userController.deleteUser)
+
+//Post
+routes.get('/posts', postController.list)
+routes.get('/post/:id', postController.show)
+routes.post('/post', postController.create)
+routes.put('/post/:id', postController.update)
+routes.delete('/post/:id', postController.deletePost)
+
+//Comment
+routes.get('/comments', commentController.list)
+routes.post('/comment', commentController.create)
+routes.put('/comment/:id', commentController.update)
+routes.delete('/comment/:id', commentController.deleteComment)
 
 
 module.exports = routes
