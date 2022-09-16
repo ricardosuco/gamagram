427779569905783dfa9bbda7ef36073ada9b4526
Ricardo Avelino <rick58@ymail.com>
2022-09-15 23:06:25 -0300

    fix: ajusta controller user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,46 +2,63 @@ const knexfile = require("../knexfile");
 const knex = require("knex")(knexfile);
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { uploadImage, deleteImage } = require('../services');
 
-const list = async (req, res) => {};
+const list = async (req, res) => {
+  const { search } = req.query;
+  try {
+    let users
+    if (search) {
+      users = await knex("users").select("name", "username", "image")
+        .where("name", "like", `%${search}%`)
+        .orWhere("username", "like", `%${search}%`)
+        .orWhere("email", "like", `%${search}%`);
+    } else {
+      users = await knex("users").select("name", "username", "image");
+    }
+    res.send(users);
+  } catch (error) {
+    res.status(500).json({ message: "Ocorreu um erro inesperado" });
+    console.log(error);
+  }
+};
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    if (!username.trim() || !password.trim()) res.status(400).json({ message: "O email é obrigatório" });
-    
-    try {
-        const foundUser = await knex("users")
-        .where("email", username)
-        .orWhere("username", username)
-        .first();
-      if (!foundUser) {
-        return res
-          .status(400)
-          .json({ message: "Email ou senha inválido" });
-      }
-    
-      const verifyPassword = await bcrypt.compare(req.body.password, foundUser.password);
-      if (!verifyPassword) {
-          return res
-              .status(400)
-              .json({ message: "Email ou senha inválido" });
-      }
+  if (!username.trim() || !password.trim())
+    res.status(400).json({ message: "O email é obrigatório" });
 
-      const token = jwt.sign({ id: foundUser.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-      const { password, ...user } = foundUser
-      res.status(200).json({ user, token });
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: "Ocorreu um erro inesperado" });
+  try {
+    const foundUser = await knex("users")
+      .where("email", username)
+      .orWhere("username", username)
+      .first();
+    if (!foundUser) {
+      return res.status(400).json({ message: "Email ou senha inválido" });
     }
 
+    const verifyPassword = await bcrypt.compare(
+      req.body.password,
+      foundUser.password
+    );
+    if (!verifyPassword) {
+      return res.status(400).json({ message: "Email ou senha inválido" });
+    }
 
-}
+    const token = jwt.sign({ id: foundUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const { password, ...user } = foundUser;
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Ocorreu um erro inesperado" });
+  }
+};
 
 const show = async (req, res) => {
-  const { username } = req.body;
+  const { username } = req.params;
   try {
     let user = await knex("users")
       .where("email", username)
@@ -62,18 +79,17 @@ const create = async (req, res) => {
   const { name, image, username, email, password, site, bio, phone, gender } =
     req.body;
 
-  !name.trim() &&
-    res.status(400).json({ message: "O campo nome é obrigatório" });
-  !username.trim() &&
-    res.status(400).json({ message: "O usename nome é obrigatório" });
-  !email.trim() &&
-    res.status(400).json({ message: "O email nome é obrigatório" });
-  !password.trim() &&
-    res.status(400).json({ message: "O password nome é obrigatório" });
-  password.trim().length < 6 &&
-    res
-      .status(400)
-      .json({ message: "A senha deve ter no mínimo 6 caracteres" });
+  if (!name.trim()){
+    return  res.status(400).json({ message: "O campo nome é obrigatório" });
+  } else if (!username.trim()){
+    return res.status(400).json({ message: "O usename nome é obrigatório" });
+  } else if (!email.trim()){
+    return res.status(400).json({ message: "O email nome é obrigatório" });
+  } else if (!password.trim()){
+    return res.status(400).json({ message: "O password nome é obrigatório" });
+  } else if (password.trim().length <  6 ){
+    return res.status(400).json({ message: "A senha deve ter no mínimo 6 caracteres" });
+  }
 
   try {
     //
@@ -84,7 +100,7 @@ const create = async (req, res) => {
     if (verifyEmailAndUsername) {
       return res
         .status(400)
-        .json({ message: "Email ou username já cadastrado" });
+        .json({ message: "Email ou username já existem" });
     }
 
     const encriptedPassword = await bcrypt.hash(password, 10);
@@ -101,32 +117,30 @@ const create = async (req, res) => {
       gender,
     });
 
-    res.status(201).json({ message: "Usuário cadastrado com sucesso" });
+    return res.status(201).json({ message: "Usuário cadastrado com sucesso" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Ocorreu um erro inesperado" });
+    return res.status(500).json({ message: "Ocorreu um erro inesperado" });
   }
 };
 const update = async (req, res) => {};
 
-const deleteUser = async (req, res) => {};
+const deleteUser = async (req, res) => {
+  const { id } = req.user;
 
-const teste = async (req, res) => {
-  let query = req.query.v;
   try {
-    let user = await knex("users")
-      .where("email", query)
-      .orWhere("username", query)
-      .first();
-    if (!user) {
-      return res.status(400).json({ message: "Usuário não encontrado" });
+    const deletedUser = await knex("users").where("id", id).del();
+    if (!deletedUser) {
+      return res
+        .status(400)
+        .json({ message: "Não foi possível deletar este usuário" });
     }
-    const { password, ...foundUser } = user;
-    res.send(foundUser);
+
+    return res.status(200).json({ message: "Usuário deletado com sucesso" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Ocorreu um erro inesperado" });
+    return res.status(500).json({ message: "Ocorreu um erro inesperado" });
   }
 };
 
-module.exports = { list, show, create, teste, update, deleteUser, login };
+module.exports = { list, show, create, update, deleteUser, login };
