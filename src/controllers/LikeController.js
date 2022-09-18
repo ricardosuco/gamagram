43f059a780100206b3112555c7c4c7a6951e31a6
Ricardo Avelino <rick58@ymail.com>
2022-09-18 14:52:41 -0300

    feat: implementa controller para like

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,10 +1,23 @@
 const knexfile = require("../knexfile");
 const knex = require("knex")(knexfile);
 
-const list = async (req, res) => { };
+const create = async (req, res) => {
+    const { id: user_id } = req.user
+    const { post_id } = req.params
 
-const create = async (req, res) => { };
+        const like = await knex("likes").insert({user_id, post_id})
+        if (!like) {
+            return res.status(400).json({message: "Não foi possível dar like no post"})
+        }
+        try {
+        return res.status(200).json({message: "ok"})
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: "Ocorreu um erro inesperado"})
+    }
+ };
 
-const update = async (req, res) => { };
 
-module.exports = { list, create, update };
+
+module.exports = { create };
